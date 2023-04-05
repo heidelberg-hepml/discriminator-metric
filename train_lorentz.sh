f58dc4902195e8239dda94d3e85315fff068a839
Ranit Das <ranit@physics.rutgers.edu>
2023-04-05 15:34:16 -0400

    train lorentzNet 10 times to check training variance for val AUC

--- train_lorentz.sh
+++ train_lorentz.sh
@@ -3,9 +3,13 @@
 
 # This script is used to train the Lorentz model on the JetNet dataset
 
+for trial in 1 2 3 4 5 6 7 8 9 
+do
+        echo "Trial $trial"
+        python -m torch.distributed.launch --nproc_per_node=2 \
+                train_lorentz.py --batch_size=32 --epochs=35  --warmup_epochs=5 \
+                --seed $((trial*2)) --n_layers=6 --n_hidden=72 --lr=0.001 \
+                --c_weight=0.001 --dropout=0.2 --weight_decay=0.01 \
+                --exp_name=trial_$trial_remove_tail --datadir=data --logdir=output
 
-python -m torch.distributed.launch --nproc_per_node=2 \
-        train_lorentz.py --batch_size=32 --epochs=35  --warmup_epochs=5 \
-        --n_layers=6 --n_hidden=72 --lr=0.001 \
-        --c_weight=0.001 --dropout=0.2 --weight_decay=0.01 \
-        --exp_name=remove_tail --datadir=data --logdir=output
+done
