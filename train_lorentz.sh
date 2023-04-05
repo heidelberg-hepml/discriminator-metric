d7e2e4db74d6ee1849d8b9a7ca81fd7ea3fcb3ac
Ranit Das <ranit@physics.rutgers.edu>
2023-04-05 15:34:48 -0400

    fixed some bugs

--- train_lorentz.sh
+++ train_lorentz.sh
@@ -10,6 +10,6 @@ do
                 train_lorentz.py --batch_size=32 --epochs=35  --warmup_epochs=5 \
                 --seed $((trial*2)) --n_layers=6 --n_hidden=72 --lr=0.001 \
                 --c_weight=0.001 --dropout=0.2 --weight_decay=0.01 \
-                --exp_name=trial_$trial_remove_tail --datadir=data --logdir=output
+                --exp_name=trial_${trial}_remove_tail --datadir=data --logdir=output
 
 done
