6f51df22f4207ce05fab6b4b8b7bd4c01bc0e3e1
Ranit Das <ranit@physics.rutgers.edu>
2023-04-08 09:14:58 -0400

    Pre run commit

--- train_particlenet.sh
+++ train_particlenet.sh
@@ -5,12 +5,12 @@
 # The .awkd files are created by notebooks/convert_dataset.ipynb 
 
 
-for trial in 1 
+for trial in 1 2 3 4 5 6 7 8 9 10
 do
     python -m train_particlenet --gpu 1 --train_dir data/converted/train_file.awkd \
         --val_dir data/converted/val_file.awkd --model_type particle_net \
-        --epochs 2 --exp_name test_${trial} \
-        --wandb_project test \
-        --wandb_group test_particlenet \
+        --epochs 20 --exp_name trial_${trial}_remove_tail_PN \
+        --wandb_project discr-metric \
+        --wandb_group remove_tail_distribution \
         --wandb_job_type particlenet 
 done
